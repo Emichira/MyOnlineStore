928c2fb2417bcb3702bc87116bf91c1ea7ca7a6a
EMichira <emmanuelmichira@gmail.com>
2017-09-10 13:58:50 +0300

    Added custom font

--- app/src/main/java/ke/co/emichira/myonlinestore/MainActivity.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/MainActivity.java
@@ -1,19 +1,25 @@
 package ke.co.emichira.myonlinestore;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindCategoriesButton;
+    private TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
 
         mFindCategoriesButton = (Button) findViewById(R.id.findCategoriesButton);
 
