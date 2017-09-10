e4a671bed435b7daa3c7778ec0ce692c3fb629f0
EMichira <emmanuelmichira@gmail.com>
2017-09-10 15:19:27 +0300

    Created a new custom adapter

--- app/src/main/java/ke/co/emichira/myonlinestore/CategoriesActivity.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/CategoriesActivity.java
@@ -1,13 +1,27 @@
 package ke.co.emichira.myonlinestore;
 
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
-public class CategoriesActivity extends DialogFragment {
+public class CategoriesActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(HelloGridView.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/ke/co/emichira/myonlinestore/ImageAdapter.java
@@ -0,0 +1,10 @@
+package ke.co.emichira.myonlinestore;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by michira on 9/10/17.
+ */
+
+public class ImageAdapter extends BaseAdapter{
+}
