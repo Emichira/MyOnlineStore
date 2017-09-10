07ee78c60620387bd628f63bbe03d5ad170e858c
EMichira <emmanuelmichira@gmail.com>
2017-09-10 13:10:02 +0300

    Created a new activity called Categories and added an Intent to navigate to it

--- app/src/main/java/ke/co/emichira/myonlinestore/MainActivity.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/MainActivity.java
@@ -1,7 +1,10 @@
 package ke.co.emichira.myonlinestore;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFindCategoriesButton = (Button) findViewById(R.id.findCategoriesButton);
+
+        mFindCategoriesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
+                startActivity(intent);            }
+        });
 
     }
 }
