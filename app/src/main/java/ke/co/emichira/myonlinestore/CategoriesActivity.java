1b0f24711b53df9b805e2ed3d08f90931c576f76
EMichira <emmanuelmichira@gmail.com>
2017-09-10 14:50:07 +0300

    Added sample images for gridview and a fragment file

--- app/src/main/java/ke/co/emichira/myonlinestore/CategoriesActivity.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/CategoriesActivity.java
@@ -2,16 +2,12 @@ package ke.co.emichira.myonlinestore;
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class CategoriesActivity extends DialogFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-        getDialog().setTitle("Simple Dialog");
-        return rootView;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
     }
 }
--- app/src/main/java/ke/co/emichira/myonlinestore/MainActivity.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/MainActivity.java
@@ -1,6 +1,7 @@
 package ke.co.emichira.myonlinestore;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
