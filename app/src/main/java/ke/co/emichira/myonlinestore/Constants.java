6842e8755761d67a3ac6f1c72c5340038fe23a07
EMichira <emmanuelmichira@gmail.com>
2017-09-22 12:37:40 +0300

    Refactored base url to retrieve trending products

--- app/src/main/java/ke/co/emichira/myonlinestore/Constants.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/Constants.java
@@ -6,7 +6,9 @@ package ke.co.emichira.myonlinestore;
 
 public class Constants {
     public static final String WALMART_API_KEY = BuildConfig.WALMART_API_KEY;
-    public static final String WALMART_BASE_URL = "http://api.walmartlabs.com/v1/search?format=json&apiKey=thzqckvaj724vmkm97wuyd6c&query=human hair extensions";
+    public static final String WALMART_BASE_URL = "http://api.walmartlabs.com/v1/trends?apiKey=thzqckvaj724vmkm97wuyd6c&lsPublisherId=xyz&format=json";
     public static final String WALMART_QUERY = "query";
     public static final String API_KEY_BASE= "apiKey";
 }
+
+
