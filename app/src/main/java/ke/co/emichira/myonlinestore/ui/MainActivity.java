0cf2caae3d939c87fcc2ab133bf4eeb46a101918
EMichira <emmanuelmichira@gmail.com>
2017-09-26 11:11:28 +0300

    Implemented Firebase and added logout menu option

--- app/src/main/java/ke/co/emichira/myonlinestore/ui/MainActivity.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/ui/MainActivity.java
@@ -3,24 +3,34 @@ package ke.co.emichira.myonlinestore.ui;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import ke.co.emichira.myonlinestore.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Bind(R.id.findCategoriesButton) Button mFindCategoriesButton;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
-    @Bind(R.id.signInButton) Button mSignInButton;
-//    @Bind(R.id.signUpButton) Button mSignUpButton;
+//    @Bind(R.id.signInButton) Button mSignInButton;
     @Bind(R.id.savedCategoriesButton) Button mSavedRestaurantsButton;
+//    @Bind(R.id.signUpButton) Button mSignUpButton;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,48 +41,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
 
-//        mFindCategoriesButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String categories = mFindCategoriesButton.getText().toString();
-//                Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
-//                intent.putExtra("categories", categories);
-//                startActivity(intent);            }
-//        });
-
         mFindCategoriesButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
 
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-                startActivity(intent);            }
-        });
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
+                } else {
 
-//        mSignUpButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-//                startActivity(intent);            }
-//        });
-
+                }
+            }
+        };
     }
 
     @Override
-    public void onClick(View v) {
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
-            if(v == mFindCategoriesButton) {
-//                String categories = mFindCategoriesButton.getText().toString();
-                Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
-//                intent.putExtra("categories", categories);
-                startActivity(intent);
-            }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    public void onClick (View v){
+
+        if (v == mFindCategoriesButton) {
+            Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
+            startActivity(intent);
+        }
 
         if (v == mSavedRestaurantsButton) {
             Intent intent = new Intent(MainActivity.this, SavedCategoriesActivity.class);
             startActivity(intent);
         }
+//        if (v == mSignInButton) {
+//            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+//            startActivity(intent);
+//        }
     }
 }
