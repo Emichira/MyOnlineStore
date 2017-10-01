f9515e18af301e0293da45c00849f69c73c242ed
EMichira <emmanuelmichira@gmail.com>
2017-10-01 12:53:46 +0300

    Added a bubble animation button

---
+++ app/src/main/java/ke/co/emichira/myonlinestore/MyBounceInterpolator.java
@@ -0,0 +1,20 @@
+package ke.co.emichira.myonlinestore;
+
+/**
+ * Created by michira on 10/1/17.
+ */
+
+public class MyBounceInterpolator implements android.view.animation.Interpolator {
+    double mAmplitude = 1;
+    double mFrequency = 10;
+
+    public MyBounceInterpolator(double amplitude, double frequency) {
+        mAmplitude = amplitude;
+        mFrequency = frequency;
+    }
+
+    public float getInterpolation(float time) {
+        return (float) (-1 * Math.pow(Math.E, -time/ mAmplitude) *
+                Math.cos(mFrequency * time) + 1);
+    }
+}
--- app/src/main/java/ke/co/emichira/myonlinestore/ui/SignInActivity.java
+++ app/src/main/java/ke/co/emichira/myonlinestore/ui/SignInActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import ke.co.emichira.myonlinestore.R;
+import ke.co.emichira.myonlinestore.MyBounceInterpolator;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
@@ -30,4 +34,16 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             finish();
         }
     }
+
+    public void didTapButton(View view) {
+        Button button = (Button)findViewById(R.id.passwordLoginButton);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+
+        button.startAnimation(myAnim);
+    }
+
 }
